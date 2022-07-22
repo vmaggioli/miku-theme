9905ca815f2b211b3a6530bf3f9630c2391bf873
vmaggioli <vinmags16@gmail.com>
2022-07-22 07:56:02 -0400

    More dereferences

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -20,13 +20,13 @@ end
 function prompt_git -d "Gets current git branch"
   if command git rev-parse --is-inside-work-tree > /dev/null 2>&1
     set -l branch (command git show-ref --head -s --abbrev | head -n1 2> /dev/null)
-    prompt_segment fff500 branch
+    prompt_segment fff500 $branch
   end
 end
 
 function prompt_path -d "Gets current path"
   set -l path (command pwd)
-  prompt_segment 9438c9 path
+  prompt_segment 9438c9 $path
 end
 
 function prompt_user -d "Prompts current user and machine"
@@ -39,9 +39,9 @@ end
 
 function fish_prompt
   prompt_segment normal "\U1F3B5"
-  add_divider
   prompt_path
   add_divider
   prompt_git
-  prompt_segment normal "\n  \U1F3A4"
+  add_divider
+  prompt_segment normal "\n  \U1F3A4 "
 end
