eda557365b3e532b3ad9a190582c9eefa9ea492b
vmaggioli <vinmags16@gmail.com>
2022-07-23 09:52:42 -0400

    Pars

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -13,7 +13,7 @@ Is the one aiming at my life
 Really an enemy?"
   set quotes[5] "The things you can’t do as one
 If there’s two, they certainly can be overcome"
-  set ran random 1 1 5
+  set -l ran (random 1 1 5)
   set_color --bold #0396c7
   echo $quotes[$ran]
   set_color normal
